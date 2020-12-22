f1c632461868273b74ba2cc29644b1b50baf3e4f
CrisDgrnu <cris.dgrnu@gmail.com>
2020-12-22 15:55:01 +0100

    Update

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import subprocess
 
 data = subprocess.check_output(['netsh','wlan','show','profiles']).decode('utf-8').split('\n')
-wifis = [line.split(':')[1][1:-1] for line in data if "All User Profile" in line]
+wifis = [line.split(':')[1][1:-1] for line in data if "Perfil de todos los usuarios" in line]
 
 for wifi in wifis:
-    results = subprocess.check_output(['netsh','wlan','show','profile',wifi,'key=clear']).decode('utf-8').split('\n')
-    result = [line.split(':')[1][1:-1] for line in results if "Key Content" in line] 
+    results = subprocess.check_output(['netsh','wlan','show','profile',wifi,'key=clear']).decode('Windows-1252').split('\n')
+    result = [line.split(':')[1][1:-1] for line in results if "Contenido de la clave" in line] 
     try:
         print(f'name:{wifi},Password:{results[0]}')
     except IndexError:
